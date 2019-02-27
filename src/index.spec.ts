a052252ad97a22b52531438cba543d8f4de74579
Lukas Kopenec <lukas@salsitasoft.com>
2019-02-27 15:19:50 +0100

    Implemented the fetch backend.

---
+++ src/index.spec.ts
@@ -0,0 +1,123 @@
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
+import { HttpErrorResponse, HttpRequestOptions, NamedValues } from 'ts-rest-client';
+
+import { FetchHttpService } from '.';
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+// tslint:disable-next-line:no-var-requires
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
+
+describe('FetchHttpService', () => {
+  const service = new FetchHttpService();
+
+  beforeEach(() => {
+    customGlobal.fetch.resetMocks();
+  });
+
+  it('Gets data using fetch', done => {
+    customGlobal.fetch.mockResponseOnce(JSON.stringify({ data: '12345' }), { headers: { 'Content-Type': 'application/json' } });
+
+    service.request(new HttpRequestOptions('http://example.api.com/get', 'GET')).subscribe(data => {
+      expect(data).toEqual({ data: '12345' });
+      expect(customGlobal.fetch.mock.calls.length).toEqual(1);
+      expect(customGlobal.fetch.mock.calls[0][0]).toEqual('http://example.api.com/get');
+      done();
+    });
+  });
+
+  it('Handles query string correctly', done => {
+    customGlobal.fetch.mockResponseOnce(JSON.stringify({ data: '12345' }), { headers: { 'Content-Type': 'application/json' } });
+
+    service.request(
+      new HttpRequestOptions(
+        'http://example.api.com/get?myValue=1', 'GET', null, null, new NamedValues({ 'Test': '12345', 'My-Addition': '1 + 6 = 7' }),
+      ),
+    ).subscribe(data => {
+      expect(data).toEqual({ data: '12345' });
+      expect(customGlobal.fetch.mock.calls.length).toEqual(1);
+      expect(customGlobal.fetch.mock.calls[0][0]).toEqual('http://example.api.com/get?myValue=1&Test=12345&My-Addition=1%20+%206%20=%207');
+      done();
+    });
+  });
+
+  it('Headers are sent correctly', done => {
+    customGlobal.fetch.mockResponseOnce(JSON.stringify({ data: '12345' }), { headers: { 'Content-Type': 'application/json' } });
+    service.request(
+      new HttpRequestOptions(
+        'http://example.api.com/get?myValue=1', 'GET', null, new NamedValues({ 'Test': '12345', 'My-Addition': '1 + 6 = 7' }),
+      ),
+    ).subscribe(data => {
+      expect(data).toEqual({ data: '12345' });
+      expect(customGlobal.fetch.mock.calls.length).toEqual(1);
+
+      const initObject = customGlobal.fetch.mock.calls[0][1];
+
+      expect(initObject.headers.get('Test')).toBe('12345');
+      expect(initObject.headers.get('My-Addition')).toBe('1 + 6 = 7');
+      done();
+    });
+  });
+
+  it('Handles text response body', done => {
+    customGlobal.fetch.mockResponseOnce('You are doomed', { headers: { 'Content-Type': 'text/plain' } });
+    service.request(
+      new HttpRequestOptions('http://example.api.com/get', 'GET'),
+    ).subscribe(data => {
+      expect(data).toEqual('You are doomed');
+      expect(customGlobal.fetch.mock.calls.length).toEqual(1);
+      done();
+    });
+  });
+
+  it('Handles error response', done => {
+    customGlobal.fetch.mockResponseOnce(
+      JSON.stringify({ message: 'You are doomed' }),
+      { headers: { 'content-type': 'application/json', 'custom': 'Test' }, status: 400, statusText: 'Shit happens' },
+    );
+    service.request(
+      new HttpRequestOptions('http://example.api.com/get', 'GET'),
+    ).subscribe(
+      () => { throw new Error('The observable should have thrown'); },
+      err => {
+        expect(err instanceof HttpErrorResponse).toBeTruthy();
+        expect(err.status).toBe(400);
+        expect(err.statusText).toBe('Shit happens');
+        expect(err.headers).toEqual({ 'content-type': 'application/json', 'custom': 'Test' });
+        expect(err.error).toEqual({ message: 'You are doomed' });
+        done();
+      },
+    );
+  });
+
+  it('Handles client-side error', done => {
+    customGlobal.fetch.mockReject(() => Promise.reject(new ProgressEvent('error', { loaded: 0, lengthComputable: false, total: 0 })));
+    service.request(
+      new HttpRequestOptions('http://example.api.com/get', 'GET'),
+    ).subscribe(
+      () => { throw new Error('The observable should have thrown'); },
+      err => {
+        expect(err instanceof HttpErrorResponse).toBeTruthy();
+        expect(err.status).toBe(0);
+        expect(err.error instanceof ProgressEvent).toBeTruthy();
+        done();
+      },
+    );
+  });
+
+  it('Handles client-side error with non Event type', done => {
+    customGlobal.fetch.mockReject(new TypeError('Wrong header buddy'));
+    service.request(
+      new HttpRequestOptions('http://example.api.com/get', 'GET'),
+    ).subscribe(
+      () => { throw new Error('The observable should have thrown'); },
+      err => {
+        expect(err instanceof HttpErrorResponse).toBeTruthy();
+        expect(err.status).toBe(0);
+        expect(err.error instanceof ErrorEvent).toBeTruthy();
+        expect((err.error as ErrorEvent).error instanceof TypeError).toBeTruthy();
+        done();
+      },
+    );
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { Observable, Observer } from 'rxjs';
+import {
+  HttpErrorResponse,
+  HttpRequestOptions,
+  HttpService,
+  StringMap,
+} from 'ts-rest-client';
+
+export class FetchHttpService implements HttpService {
+  constructor(requestInit?: RequestInit) {
+    this.requestInit = requestInit || {};
+  }
+
+  private requestInit: RequestInit;
+
+  request(options: HttpRequestOptions): Observable<any> {
+    return Observable.create(async (observer: Observer<any>) => {
+      const requestUrl = options.getUrl();
+
+      try {
+        const response = await fetch(requestUrl, {
+          ...this.requestInit,
+          body: options.getSerializedBody(),
+          headers: new Headers(options.headers.values),
+          method: options.method,
+        });
+
+        if (!response.ok) {
+          const error = await this.getResponseBody(response);
+          const headers = {} as StringMap;
+          if (response.headers) {
+            response.headers.forEach((value, key) => headers[key] = value);
+          }
+          observer.error(new HttpErrorResponse({
+            error,
+            headers,
+            status: response.status,
+            statusText: response.statusText,
+            url: response.url,
+          }));
+          return;
+        }
+
+        observer.next(await this.getResponseBody(response));
+        observer.complete();
+      } catch (err) {
+        let error: Event = null;
+
+        if (err instanceof Event) {
+          error = err;
+        } else {
+          error = new ErrorEvent('error', { error: err });
+        }
+
+        observer.error(new HttpErrorResponse({ error, url: requestUrl }));
+      }
+    });
+  }
+
+  getResponseBody(response: Response): Promise<any> {
+    const contentType = response.headers.get('Content-Type');
+
+    if (contentType === 'application/json') {
+      return response.json();
+    }
+
+    return response.text();
+  }
+}
